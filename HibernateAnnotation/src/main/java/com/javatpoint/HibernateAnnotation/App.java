ad95b7594c2bf94df5ff53e7942241dbc36b6cae
saurabh <saurabh.talreja@servicenow.com>
2019-08-22 13:59:34 +0530

    hibernate pocs

---
+++ HibernateAnnotation/src/main/java/com/javatpoint/HibernateAnnotation/App.java
@@ -0,0 +1,13 @@
+package com.javatpoint.HibernateAnnotation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ HibernateMapping/src/main/java/com/javatpoint/mypackage/Employee.java
@@ -0,0 +1,21 @@
+package com.javatpoint.mypackage;
+
+public class Employee {
+
+	private String name;
+	private int id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+}
